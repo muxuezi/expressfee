27f35cbca1480ab3c008848fad9d732f677a8248
muxuezi <muxuezi@gmail.com>
2014-04-13 19:18:54 +0800

    end one

--- school.py
+++ school.py
@@ -4,9 +4,9 @@
 
 from bs4 import BeautifulSoup
 import urllib2
-# import sys
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -23,9 +23,9 @@ class SchoolName(object):
         self.argp0 = argp0
         self.argp1 = argp1
         self.baseurl = "http://kaoshi.edu.sina.com.cn/abroad/list.php?country=&type=&zhinanflag=&collegename=&page="
+        self.temp = []
 
-
-    def catch_txt(self,purl):
+    def catch_txt(self, purl):
         print 'page in: %s' % str(purl + 1)
         url = self.baseurl + str(purl + 1)
         page = urllib2.urlopen(url)
@@ -34,30 +34,34 @@ class SchoolName(object):
         rows = table.findAll(lambda tag: tag.name == 'tr')
         return enumerate(rows[1:])
 
-    def contactFrame(self,url):
+    def contactFrame(self, url):
         page = urllib2.urlopen(url)
         esoup = BeautifulSoup(page.read())
         ename = esoup.find('table', attrs={'width': "98%"}).text
-        txt = ename.strip().split('\n')
+        txt = ename.strip().split('\r\n')
         if len(txt) == 1:
-            driver = webdriver.Firefox()
-            driver.implicitly_wait(10)
-            driver.get(url)
-            try:
-                driver.switch_to_frame('contactFrame')
-                name = driver.find_element_by_xpath(
-                    '/html/body/div[1]/div/div/div[2]/h4')
-            except UnexpectedAlertPresentException or NoSuchFrameException or NoSuchElementException:
-                driver.quit()
-                return 'None'
+            if 'contactFrame' in esoup.get_text():
+                driver = webdriver.Chrome('./chromedriver')
+                driver.implicitly_wait(10)
+                driver.get(url)
+                try:
+                    driver.switch_to_frame('contactFrame')
+                    name = driver.find_element_by_xpath(
+                        '/html/body/div[1]/div/div/div[2]/h4')
+                except UnexpectedAlertPresentException or NoSuchElementException:
+                    driver.quit()
+                    return 'None'
+                else:
+                    txt = name.text.strip().split('\r\n')
+                    driver.quit()
+                    return txt[-1].strip() if len(txt) > 1 else 'None'
             else:
-                txt = name.text.strip().split('\n')
-                driver.quit()
-                return txt[-1].strip() if len(txt) > 1 else 'None'
+                return 'None'
+
         else:
             return txt[-1].strip()
 
-    def gettext(self, page,idx, row):
+    def gettext(self, page, idx, row):
         itemid = str(page * 20 + idx + 1)
         schtext = row.text.strip().split('\n')
         url = row.find('a', attrs={'target': "_blank"}).get('href')
@@ -67,18 +71,17 @@ class SchoolName(object):
         return output
 
     def outxlsx(self):
-        temp = []
+        
         fname = 'schoolname_%s_%s.xlsx' % (str(self.argp0), str(self.argp1))
         for upage in range(self.argp0, self.argp1):
             for idx, row in self.catch_txt(upage):
                 schoolinfo = self.gettext(upage, idx, row)
-                print schoolinfo[0]
-                temp.append(schoolinfo)
-        df = pd.DataFrame(
-            temp, columns=['id', u'院校名称', u'中文名称', u'国家', u'城市', u'类别', 'url',''])
+                print schoolinfo[0],schoolinfo[2]
+                self.temp.append(schoolinfo)
+        df = pd.DataFrame(self.temp, columns=['id', u'院校名称', u'中文名称', u'国家', u'城市', u'类别', 'url', ''])
         df.to_excel(fname, u'国外高校', index=False)
 
 
 if __name__ == '__main__':
-    s = SchoolName(600, 678)
+    s = SchoolName(0,678)
     s.outxlsx()
